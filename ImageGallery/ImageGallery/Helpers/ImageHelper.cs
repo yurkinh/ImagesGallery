89125e834157d32dd8cfaf67b4d3c95eaf08b052
Yuriy Holembyovskyy <yholembyovskyy@gmail.com>
2021-11-16 11:50:52 +0200

    Initial commit; Caching for android implemented with GlideX; Caching fro ios implemented with Nuke; Both are native libraries which leads to optimal performance on both platforms

---
+++ ImageGallery/ImageGallery/Helpers/ImageHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ImageGallery.Helpers
+{
+    public static  class ImageHelper
+    {
+		static readonly Random random = new();		
+		
+		public static IEnumerable<ImageSource> GetPicsumImages(int count)
+		{
+			var width = (int)(DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density);
+			var url = $"https://picsum.photos/{width}/200";
+
+			for (int i = 0; i < count; i++)
+			{
+				yield return ImageSource.FromUri(new Uri($"{url}?{random.Next()}"));
+			}
+		}
+	}
+}
---
+++ ImageGallery/ImageGallery/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using ImageGallery.Helpers;
+using Xamarin.Forms;
+
+namespace ImageGallery
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly ObservableRangeCollection<ImageSource> images = new(ImageHelper.GetPicsumImages(5).ToArray());
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = images;
+            //Using Glidex for Android imasge caching
+            //Using Nuke for ios image caching
+        }
+
+        void CollectionView_RemainingItemsThresholdReached(object sender, EventArgs e)=>
+            images.AddRange(ImageHelper.GetPicsumImages(5).ToArray());
+
+    }
+}
